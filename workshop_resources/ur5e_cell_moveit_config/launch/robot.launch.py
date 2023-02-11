0f3d256057362f001f04c5900af04847d5fdd39c
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2023-02-11 19:52:59 +0000

    Update robot.launch.py
    Signed-off-by: Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>

--- workshop_resources/ur5e_cell_moveit_config/launch/robot.launch.py
+++ workshop_resources/ur5e_cell_moveit_config/launch/robot.launch.py
@@ -1,27 +1,19 @@
-from os.path import join
-import pprint
 from launch import LaunchDescription
-from moveit_configs_utils import MoveItConfigsBuilder
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.substitutions import FindPackageShare
+from launch_ros_extras.actions import LoadMoveitConfig, GenerateMoveitLaunch
+
+from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg
+
 from moveit_configs_utils.launches import (
     generate_move_group_launch,
     generate_moveit_rviz_launch,
-    generate_rsp_launch,
-    generate_spawn_controllers_launch,
-    generate_static_virtual_joint_tfs_launch,
-    generate_warehouse_db_launch
 )
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import UnlessCondition, IfCondition
-from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg
-from launch.substitutions import LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
-from launch_ros_extras.actions import LoadMoveitConfig, GenerateMoveitLaunch, LoadYaml
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+
 
 def generate_launch_description():
     
@@ -72,7 +64,7 @@ def generate_launch_description():
             "use_fake_hardware": "false",
             "initial_joint_controller": "joint_trajectory_controller",
             "activate_joint_controller": "true",
-            "robot_ip": "192.168.56.2",
+            "robot_ip": LaunchConfiguration("robot_ip"),
             "description_package": "ur5e_cell_description",
             "description_file": "workcell.urdf.xacro",
             "launch_rviz": "false"
